cd6f2adc52e363472dcaba04097db69ff3d7dca4
Joe-Wayne Davis <joewaynejdavis@gmail.com>
2020-02-20 01:35:29 -0500

    waiting

--- lib/utils/Sqlite/db_helper.dart
+++ lib/utils/Sqlite/db_helper.dart
@@ -25,4 +25,34 @@ initDB() async{
 
 }
 
+_onCreate(Database db,int version) async{
+  await db
+          .execute("CREATE TABLE $TABLE($ID INTEGER PRIMARY KEY, $NAME TEXT");
+}
+
+Future<item> save (item item) async{
+  var dbClient = await db;
+  item.id = await dbClient.insert(TABLE, item.toMap());
+   return item;
+/*
+  await dbClient.transaction((txn) async{
+    var query = "INSERT INTO $TABLE($NAME) VALUES ('"+ item.name + "')";
+    return await txn,rawInsert(query);
+  });
+  */
+
+}
+
+Future<List<item>> getItems() async{
+  var dbClient = await db;
+  List <Map> maps = await dbClient.query(TABLE, columns:[ID, NAME]);
+ // List<Map> maps = await dbClient.rawQuery("SELECT * FROM $TABLE");
+ List<item> items = [];
+ if( maps.length> 0) {
+   for (int i=0; i< maps.length; i++){
+     items.add(item.fromMap(maps[i]));
+   }
+ }
+return items;
+}
 }
